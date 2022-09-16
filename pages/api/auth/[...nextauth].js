771291d841c670a55caca9427d972bf166a0c7af
Dave Kiss <dkiss@mux.com>
2022-09-16 13:14:29 -0400

    Add auth to db schema

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,16 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { prisma } from "utils/prisma"
+
 export const authOptions = {
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // ...add more providers here
+    })
   ],
 }
 
