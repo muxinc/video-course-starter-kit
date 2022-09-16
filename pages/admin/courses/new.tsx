0018d3a74173cd7b9b52c8148b1c9b3da9019e89
Dave Kiss <dkiss@mux.com>
2022-09-16 14:29:30 -0400

    Wire up a basic form

--- pages/admin/courses/new.tsx
+++ pages/admin/courses/new.tsx
@@ -1,15 +1,30 @@
 import type { NextPage } from 'next'
 import { prisma } from 'utils/prisma'
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string,
+};
+
 
 const AdminNewCourse: NextPage = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
   return (
     <>
       <h1 className="text-4xl text-center font-bold">
         New course
       </h1>
+      <form className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="name">Name</label>
+        <input className='bg-gray-100' {...register("name", { required: true })} />
+        {errors.name && <span>Name is required</span>}
+
+        <input type="submit" value='Create course' />
+      </form>
     </>
-  )
+  );
 
 }
 
