754a2e79c676d0d6c1e972c502b6da4375d43afc
Dave Kiss <dkiss@mux.com>
2022-09-19 15:22:44 -0400

    Get all them duggies in there

--- environment.d.ts
+++ environment.d.ts
@@ -1,10 +1,13 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    DATABASE_URL: string;
+    DB_USER: string;
+    DB_PASS: string;
     GITHUB_ID: string;
     GITHUB_SECRET: string;
     MUX_TOKEN_ID: string;
     MUX_TOKEN_SECRET: string;
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: string; // this is the line you want
+    NEXTAUTH_URL: string;
     NODE_ENV: 'development' | 'production';
     PORT?: string;
     PWD: string;
