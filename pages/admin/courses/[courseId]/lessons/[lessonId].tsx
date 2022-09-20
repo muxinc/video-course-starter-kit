62a54ecc8c065bc9d1d0d92f6e2b173673e16aef
Dave Kiss <dkiss@mux.com>
2022-09-20 14:30:09 -0400

    Add video to lesson page

--- pages/admin/courses/[courseId]/lessons/[lessonId].tsx
+++ pages/admin/courses/[courseId]/lessons/[lessonId].tsx
@@ -5,12 +5,16 @@ import { GetServerSideProps } from 'next'
 import { authOptions } from 'pages/api/auth/[...nextauth]'
 import { unstable_getServerSession } from "next-auth/next"
 import type { Session } from 'next-auth'
-import type { Course, Lesson } from '@prisma/client'
+import type { Course, Lesson, Video } from '@prisma/client'
 import Link from 'next/link'
+
+import MuxPlayer from "@mux/mux-player-react";
 
 type AdminLessonEditPageProps = {
   session: Session;
-  lesson: Lesson;
+  lesson: Lesson & {
+    video: Video | null;
+  }
 }
 
 const AdminLessonEdit: NextPage<AdminLessonEditPageProps> = ({ lesson }) => {
@@ -21,6 +25,11 @@ const AdminLessonEdit: NextPage<AdminLessonEditPageProps> = ({ lesson }) => {
       <>
         <h2 className='text-xl'>{lesson.name}</h2>
         <p>{lesson.description}</p>
+
+        <MuxPlayer
+          streamType="on-demand"
+          playbackId={lesson.video?.publicPlaybackId}
+        />
       </>
     )
   }
@@ -49,10 +58,13 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       id: parseInt(id),
       course: {
         author: {
-          email: session.user?.email
+          id: session.user?.id
         }
       }
     },
+    include: {
+      video: true
+    }
   })
 
   if (!lesson) {
