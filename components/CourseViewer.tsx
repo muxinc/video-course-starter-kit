8609eef567752e475d63f8dda68cd630cfa5f0a1
Dave Kiss <dkiss@mux.com>
2022-09-26 13:30:39 -0400

    Getting layouts squared away

--- components/CourseViewer.tsx
+++ components/CourseViewer.tsx
@@ -15,48 +15,55 @@ type Props = {
 }
 
 const CourseViewer = ({ course }: Props) => {
-  const [playbackId, setPlaybackId] = useState(course.lessons[0].video?.publicPlaybackId)
+  const [activeLesson, setActiveLesson] = useState(course.lessons[0]);
+  const playbackId = activeLesson.video?.publicPlaybackId
 
   return (
-    <>
-      <MuxPlayer
-        className='mb-6'
-        streamType="on-demand"
-        playbackId={playbackId}
-      />
+    <div className='grid grid-cols-[70%_30%]'>
+      <div>
+        <MuxPlayer
+          className='mb-6'
+          streamType="on-demand"
+          playbackId={playbackId}
+        />
+        <Heading>{activeLesson.name}</Heading>
+        <p>{activeLesson.description}</p>
+      </div>
 
-      {course.lessons.map(lesson => (
-        <a
-          onClick={() => setPlaybackId(lesson.video?.publicPlaybackId)}
-          key={lesson.id}
-          className={clsx({
-            'flex gap-6 cursor-pointer hover:bg-gray-50 py-4': true,
-            'bg-yellow-50': playbackId === lesson.video?.publicPlaybackId
-          })}
-        >
-          {lesson.video?.publicPlaybackId && (
-            <div className='w-32 h-[4.5rem] rounded border'>
-              <Image
-                className='w-full h-full object-contain'
-                src={`https://image.mux.com/${lesson.video.publicPlaybackId}/thumbnail.jpg?width=640`}
-                alt={`Video thumbnail preview for ${lesson.name}`}
-                width={320}
-                height={240}
-              />
+      <div>
+        {course.lessons.map(lesson => (
+          <a
+            onClick={() => setActiveLesson(lesson)}
+            key={lesson.id}
+            className={clsx({
+              'flex gap-6 cursor-pointer hover:bg-gray-50 py-4': true,
+              'bg-yellow-50': playbackId === lesson.video?.publicPlaybackId
+            })}
+          >
+            {lesson.video?.publicPlaybackId && (
+              <div className='w-32 h-[4.5rem] rounded border'>
+                <Image
+                  className='w-full h-full object-contain'
+                  src={`https://image.mux.com/${lesson.video.publicPlaybackId}/thumbnail.jpg?width=640`}
+                  alt={`Video thumbnail preview for ${lesson.name}`}
+                  width={320}
+                  height={240}
+                />
+              </div>
+            )}
+            <div>
+              <h2>
+                <span className='font-semibold text-lg text-gray-800'>{lesson.name}</span>
+                {lesson.video && (
+                  <span className='text-md italic text-gray-600 truncate'> • {formatDuration(Math.round(lesson.video.duration))}</span>
+                )}
+              </h2>
+              <p className='text-md italic text-gray-600 my-1 truncate'>{lesson.description}</p>
             </div>
-          )}
-          <div>
-            <h2>
-              <span className='font-semibold text-lg text-gray-800'>{lesson.name}</span>
-              {lesson.video && (
-                <span className='text-md italic text-gray-600 truncate'> • {formatDuration(Math.round(lesson.video.duration))}</span>
-              )}
-            </h2>
-            <p className='text-md italic text-gray-600 my-1 truncate'>{lesson.description}</p>
-          </div>
-        </a>
-      ))}
-    </>
+          </a>
+        ))}
+      </div>
+    </div>
   );
 };
 
